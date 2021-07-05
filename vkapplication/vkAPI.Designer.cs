e0b0cd1bc1fd86c99c489b67e0c6c333aaa52740
Eightclockz <nikita.cozlow.0210@yandex.ru>
2021-07-05 15:05:11 +0300

    Создайте кнопку

--- vkapplication/vkAPI.Designer.cs
+++ vkapplication/vkAPI.Designer.cs
@@ -29,13 +29,24 @@ namespace vkapplication
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(469, 157);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(406, 154);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // vkAPI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1017, 498);
+            this.Controls.Add(this.button1);
             this.Name = "vkAPI";
             this.Text = "vkAPI";
             this.ResumeLayout(false);
@@ -43,6 +54,8 @@ namespace vkapplication
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
