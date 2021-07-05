24e78390d8c3bc554b3910be1c5c3909f15a8d92
Юрий Мазуров <mazurov.iurii@gmail.com>
2021-07-05 15:15:45 +0300

    button 2

--- vkapplication/vkAPI.Designer.cs
+++ vkapplication/vkAPI.Designer.cs
@@ -30,23 +30,36 @@ namespace vkapplication
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(469, 157);
+            this.button1.Location = new System.Drawing.Point(536, 209);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(406, 154);
+            this.button1.Size = new System.Drawing.Size(464, 205);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(717, 502);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 37);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // vkAPI
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1017, 498);
+            this.ClientSize = new System.Drawing.Size(1162, 664);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "vkAPI";
             this.Text = "vkAPI";
             this.ResumeLayout(false);
@@ -56,6 +69,7 @@ namespace vkapplication
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
